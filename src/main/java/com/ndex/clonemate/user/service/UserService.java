f537da3f73d9b677cbb43f8b8d0d3f0193ad08b6
yeon-06 <rnjstldus2@hs.ac.kr>
2022-01-11 23:16:57 +0900

    refactor: 하드코딩 제거

--- src/main/java/com/ndex/clonemate/user/service/UserService.java
+++ src/main/java/com/ndex/clonemate/user/service/UserService.java
@@ -8,33 +8,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final static char FLAG_YES = 'Y';
     private final static char FLAG_NO = 'N';
+    private final static String ERROR_NO_USER = "[ERROR] 해당 사용자가 없습니다.";
     private final UserRepository userRepository;
 
     public UserResponseDto findByUserId(String userId) {
         User entity = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 사용자가 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_NO_USER));
         return new UserResponseDto(entity);
     }
 
     @Transactional
     public void delete(String userId) {
         User entity = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 사용자가 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_NO_USER));
         entity.changeDeleteFlag();
     }
 
     @Transactional
     public void update(String userId, UserUpdateRequestDto request) {
         User entity = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 사용자가 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_NO_USER));
 
         entity.update(request.getUserId(), request.getEmail(), request.getName(),
                 request.getIntroText(), request.getEmailSearchYn(), request.getRandomYn());
     }
+
 }
