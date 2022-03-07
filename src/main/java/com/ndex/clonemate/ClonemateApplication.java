07ae3104575d836b04a0f08f67c128af64046850
yeonLog <53105735+yeon-06@users.noreply.github.com>
2022-03-07 21:48:54 +0900

    refactor: try-catch 감싸기

--- src/main/java/com/ndex/clonemate/ClonemateApplication.java
+++ src/main/java/com/ndex/clonemate/ClonemateApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClonemateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClonemateApplication.class, args);
+		try{
+			SpringApplication.run(ClonemateApplication.class, args);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
