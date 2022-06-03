23f3dd2dfabb70c95291394495d188294a4a5a0b
borajin <borajin.soft@gmail.com>
2022-06-03 09:16:43 +0900

    feat: 투두, 목표, 좋아요, 팔로우 등 기능 병합

--- src/main/java/com/ndex/clonemate/ClonemateApplication.java
+++ src/main/java/com/ndex/clonemate/ClonemateApplication.java
@@ -2,14 +2,16 @@ package com.ndex.clonemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy	//aop
 @SpringBootApplication
 public class ClonemateApplication {
 
 	public static void main(String[] args) {
-		try{
+		try {
 			SpringApplication.run(ClonemateApplication.class, args);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
