9a91b6cb73d4d80a6e0f3f252dc9243606d6e4ba
진보라 (Jin Bora) <55897222+borajin@users.noreply.github.com>
2022-07-09 22:43:24 +0900

    fix: cors 이슈, redis 설정 문제 해결 (#6)

--- src/main/java/com/ndex/clonemate/certificate/config/RedisConfig.java
+++ src/main/java/com/ndex/clonemate/certificate/config/RedisConfig.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @Configuration
 public class RedisConfig {
@@ -14,8 +16,23 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    //외부 Redis 연결할 때 아래 옵션을 활성화해야 함.
+    // https://somida.tistory.com/189
+    @Bean
+    ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
     }
 }
