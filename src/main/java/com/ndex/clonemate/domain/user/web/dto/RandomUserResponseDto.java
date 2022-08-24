3afb1eace2b01856805dc9ffd99f8e2203d47ee9
yeonLog <53105735+yeon-06@users.noreply.github.com>
2022-08-24 14:20:34 +0900

    feat: 사용자 랜덤 조회 시 email 값 추가 (#9)

--- src/main/java/com/ndex/clonemate/domain/user/web/dto/RandomUserResponseDto.java
+++ src/main/java/com/ndex/clonemate/domain/user/web/dto/RandomUserResponseDto.java
@@ -12,6 +12,7 @@ public class RandomUserResponseDto {
     private Long id;
     private String userId;
     private String name;
+    private String email;
     private String introText;
 
     @Builder
@@ -19,6 +20,7 @@ public class RandomUserResponseDto {
         this.id = user.getId();
         this.userId = user.getAccount();
         this.name = user.getName();
+        this.email = user.getEmail();
         this.introText = user.getIntroText();
     }
 }
